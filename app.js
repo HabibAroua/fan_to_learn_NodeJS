fcd2317774bfb85aec03384996ab32684734e109
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-02-28 20:49:40 +0100

    create a table

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ app.get('/createdb',(req,res)=>
     });
 });
 
+//Create table
+app.get('/createpoststable' , (req,res)=>
+{
+    let sql='CREATE TABLE post (id int PRIMARY KEY AUTO_INCREMENT  , title varchar(255) , body varchar(255))';
+    db.query(sql,(err,result)=>
+    {
+        if(err)throw err;
+        console.log(result);
+        res.send('Post table created ....');
+    });
+});
+
 app.listen('3000',()=>
     {
         console.log('Server started on port 3000');
