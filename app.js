eeb0e4406e22de9e723370bb49ad0b4c55d82e99
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-11 00:07:03 +0100

    update post

--- app.js
+++ app.js
@@ -83,6 +83,19 @@ app.get('/getPosts/:id',(req,res)=>
    });
 });
 
+//update post
+app.get('/update/:id',(req,res)=>
+{
+    let newTitle='Update post'
+    let sql=`UPDATE post SET title = '${newTitle}' WHERE id=${req.params.id}`;
+    let query=db.query(sql,(err,result)=>
+    {
+        if(err)throw err;
+        console.log(result);
+        res.send('Post updated ...');
+    });
+});
+
 app.listen('3000',()=>
     {
         console.log('Server started on port 3000');
