cdfcbb3ed12095d67c9bc7c649404083eb37bc1d
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-03-11 00:26:36 +0100

    edit project

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.get('/delete/:id',(req,res)=>
 });
 
 app.listen('3000',()=>
-    {
-        console.log('Server started on port 3000');
-    });
+{
+    console.log('Server started on port 3000');
+});
