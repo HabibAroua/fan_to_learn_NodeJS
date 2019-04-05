844b2cf59b407df874dbb8314143d83bd396c74f
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-04-05 21:16:49 +0100

    install package

--- App1/server.js
+++ App1/server.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//var Users=require('./routes/Users');
+var Users=require('./routes/Users');
 
-//app.use('/users',Users);
+app.use('/users',Users);
 
 app.listen(port,() =>{
     console.log("Server is running on port: "+port);
