6ae7c914ec5e54ca242ff19f95da79b134a59312
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-04-10 00:07:22 +0100

    dev button

--- App1/client/src/components/Navbar.js
+++ App1/client/src/components/Navbar.js
@@ -12,6 +12,49 @@ class Navbar extends Component
 
     render()
     {
+        const loginRegLink=(
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to='/login' className="nav-link">
+                        Login
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/register" className="nav-link">
+                        Register
+                    </Link>
+                </li>
+            </ul>
+        )
+        const userLink = (
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/profile" className="nav-link">
+                        User
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <a href="" onClick={this.logOut.bind(this)} className="nav-link">
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        )
+        return(
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+                <button className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbar1"
+                        aria-controls="navbar1"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggle-icon"></span>
+                </button>
 
+                </button>
+            </nav>
+        )
     }
 }
