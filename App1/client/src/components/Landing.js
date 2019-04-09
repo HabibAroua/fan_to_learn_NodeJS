22cf8df502e27343b21a3c8d70c5c5870d89048a
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-04-09 23:11:30 +0100

    dev Landing js

---
+++ App1/client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react'
+
+class Landing extends Component
+{
+    render()
+    {
+        return (
+            <div className="container">
+                <div className="jumbotrom mt-5">
+                    <h1 className="text-center">Welcome</h1>
+                </div>
+            </div>
+        )
+    }
+}
+export default Landing
