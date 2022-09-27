6c66aa8e10a9532fc1e3a1e6c4a7318ffab227d2
Habib Aroua <habib.a@xelero.io>
2022-09-27 14:36:43 +0100

    Update index.js

--- swagger/index.js
+++ swagger/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { listenerCount } = require("process");
 const swaggerUI = require("swagger-ui-express");
 const YAML = require('yamljs');
 const swaggerJsDocs = YAML.load('./api.yaml');
@@ -7,4 +6,4 @@ const app = express();
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
-app/listenerCount(4000, () => console.log("UP & Running"));
+app.listen(4000, () => console.log("UP & Running"));
