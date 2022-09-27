af7a1a7c74ebd29574260829180dfd69bcb66177
Habib Aroua <habib.a@xelero.io>
2022-09-27 15:09:23 +0100

    Update index.js

--- swagger/index.js
+++ swagger/index.js
@@ -6,4 +6,11 @@ const app = express();
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
+app.get('/string',(req, res)=>{ res.status(200).send("This is a String") })
+
+app.get('/user', (req, res)=>{
+    const obj = {id: 1, name: "Habib Aroua"};
+    res.status(200).send(obj)
+});
+
 app.listen(4000, () => console.log("UP & Running"));
